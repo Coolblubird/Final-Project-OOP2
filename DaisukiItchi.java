fcd1eaeb1b49dd8eb5a67306e9075a629b17d2a3
Coolblubird <80004413+Coolblubird@users.noreply.github.com>
2021-06-21 08:54:30 -0600

    updated stuff to work better

--- DaisukiItchi.java
+++ DaisukiItchi.java
@@ -11,17 +11,19 @@ import javafx.scene.shape.*;
 import javafx.scene.control.*;
 import javafx.scene.Group;
 import javafx.geometry.*;
+import javax.swing.plaf.basic.*;
 
 public class DaisukiItchi extends Application {
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {   
 		// Create a scene and place it in the stage
 		
-		Scene scene = new Scene(gui(),400,400);
+		Scene scene = new Scene(gui(),400,500);
 		primaryStage.setTitle("ShowPolygon"); // Set the stage title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.setResizable(false);
 		primaryStage.show(); // Display the stage
+		
 	}
 	
 	/**
@@ -33,8 +35,13 @@ public class DaisukiItchi extends Application {
 	}
 	
 	public Pane gui(){
-		VBox maxPane = new VBox(20.0);
+		VBox maxPane = new VBox(10.0);
 		GridPane gp = new GridPane();
+		int userAnswer = 0;
+		int random = 0;
+		
+		//dasukiitchi
+		MyMonster dai = new MyMonster();
 		
 		//the title
 		Label lebell = new Label("DaisukiItchi: By Jordan Ashe");
@@ -44,34 +51,97 @@ public class DaisukiItchi extends Application {
 		//interactable buttons
 		Button btn1 = new Button("Yes");
 		Button btn2 = new Button("No");
-		Button btn3 = new Button("S o r r y.");
-		Button btn4 = new Button("Easy Peasy!");
-		btn1.setPrefWidth(90);
-		btn2.setPrefWidth(90);
-		btn3.setPrefWidth(90);
-		btn4.setPrefWidth(90);
+		Button btn3 = new Button("Sorry...");
+		Button btn4 = new Button("Not Yet!");
+		
+		btn1.setPrefWidth(100);
+		btn2.setPrefWidth(100);
+		btn3.setPrefWidth(100);
+		btn4.setPrefWidth(100);
+		
+		//Daisuki prompt
+		Label tfP = new Label("Test");
+		tfP.setFont(Font.font("Times New Roman", 14));
+		tfP.setTextFill(Color.BLACK);
+	
+		//question prompt
+		Label tfQ = new Label("This is DaisukiItchi, do you like him?");
+		tfQ.setFont(Font.font("Times New Roman", 20));
+		tfQ.setTextFill(Color.BLACK);
+		
+		//score
+		Label score = new Label("Score: 0");
+		score.setFont(Font.font("Times New Roman", 12));
+		score.setTextFill(Color.BLACK);
+		
+		//add your own questions!
+		Label flavorTxt1 = new Label("Add your own questions!");
+		Label flavorTxt2 = new Label("(Note: it works best when they are Yes/No questions)");
+		flavorTxt1.setFont(Font.font("Times New Roman", 14));
+		flavorTxt1.setTextFill(Color.BLACK);
+		flavorTxt2.setFont(Font.font("Times New Roman", 14));
+		flavorTxt2.setTextFill(Color.BLACK);
+		TextField newQuestionTaker = new TextField();
+		
+		newQuestionTaker.setOnAction(e -> {
+			dai.setNewQuestion(newQuestionTaker.getText());
+			newQuestionTaker.setText("");
+		});
+		
+		btn1.setOnAction(e -> {
+			if (dai.questions.size()>0){
+				dai.setEmote("joy");
+				tfP.setText(dai.getText());
+				score.setText("Score: " + dai.getScore());
+			}
+			tfQ.setText(dai.getQuestion());
+			dai.newQuestion();
+		});
+		btn2.setOnAction(e -> {
+			if (dai.questions.size()>0){
+				dai.setEmote("sad");
+				tfP.setText(dai.getText());
+			}
+			tfQ.setText(dai.getQuestion());
+			dai.newQuestion();
+		});
+		btn3.setOnAction(e -> {
+			if (dai.questions.size()>0){
+				dai.setEmote("shock");
+				tfP.setText(dai.getText());
+			}
+			tfQ.setText(dai.getQuestion());
+			dai.newQuestion();
+		});
+		btn4.setOnAction(e -> {
+			if (dai.questions.size()>0){
+				dai.setEmote("wink");
+				tfP.setText(dai.getText());
+				score.setText("Score: " + dai.getScore());
+			}
+			tfQ.setText(dai.getQuestion());
+			dai.newQuestion();
+		});
+		
 		gp.add(btn1,0,0);
 		gp.add(btn2,1,0);
-		gp.add(btn3,0,1);
-		gp.add(btn4,1,1);
+		gp.add(btn3,1,1);
+		gp.add(btn4,0,1);
 		gp.setHalignment(btn1, HPos.CENTER);
 		gp.setHalignment(btn2, HPos.CENTER);
 		gp.setHalignment(btn3, HPos.CENTER);
 		gp.setHalignment(btn4, HPos.CENTER);
 		gp.setMaxWidth(200);
 		
-		//Daisuki prompt
-		Label tfP = new Label("Test");
-		tfP.setFont(Font.font("Times New Roman", 20));
-		tfP.setTextFill(Color.BLACK);
-		
-		//dasukiitchi
-		MyMonster dai = new MyMonster();
-		
 		maxPane.getChildren().add(lebell);
+		maxPane.getChildren().add(tfQ);
 		maxPane.getChildren().add(gp);
 		maxPane.getChildren().add(tfP);
 		maxPane.getChildren().add(dai);
+		maxPane.getChildren().add(score);
+		maxPane.getChildren().add(flavorTxt1);
+		maxPane.getChildren().add(flavorTxt2);
+		maxPane.getChildren().add(newQuestionTaker);
 		maxPane.setAlignment(Pos.TOP_CENTER);
 		return maxPane;
 	} 
--- MyMonster.java
+++ MyMonster.java
@@ -11,36 +11,93 @@ import javafx.scene.shape.*;
 import javafx.scene.control.*;
 import javafx.scene.Group;
 import javafx.geometry.*;
+import java.util.ArrayList;
 
 class MyMonster extends Pane {
 	String emote = "hello";
-	int joy = 0;
+	String text = "Oh! Hello!";
+	int score = 0;
+	String question = "Have you gone outside today?";
+	ArrayList<String> questions = new ArrayList<>();
+	
+	MyMonster(){
+		questions.add("Have you eaten today?");
+		questions.add("Have you seen any friends today?");
+		questions.add("Have you exercised today?");
+	}
 	
 	private void paint() {
 		// Create a polygon and place polygon to pane
 		Circle body = new Circle(200,100,80);
+		Arc mouth = new Arc();
+		Ellipse eye2 = new Ellipse();
+		eye2.setRadiusX(10.0f);
+		eye2.setRadiusY(10.0f);
+		eye2.setCenterX(240.0f);
+		eye2.setCenterY(80.0f);
+		
+		Ellipse eye1 = new Ellipse();
+		eye1.setRadiusY(10.0f);
+		eye1.setRadiusX(10.0f);
+		eye1.setCenterX(160.0f);
+		eye1.setCenterY(80.0f);
+		
 		body.setFill(Color.WHITE);
 		body.setStroke(Color.BLACK);
 		body.setStrokeWidth(5);
 		
-		Circle eye1 = new Circle(160,80,10);
 		eye1.setFill(Color.WHITE);
 		eye1.setStroke(Color.BLACK);
 		eye1.setStrokeWidth(5);
 		
-		Circle eye2 = new Circle(240,80,10);
 		eye2.setFill(Color.WHITE);
 		eye2.setStroke(Color.BLACK);
 		eye2.setStrokeWidth(5);
+
+		switch(emote){
+			case "sad":
+				eye1.setCenterY(90.0f);
+				eye2.setCenterY(90.0f);
+				
+				mouth.setCenterX(200.0f);
+				mouth.setCenterY(130.0f);
+				mouth.setRadiusX(25.0f);
+				mouth.setRadiusY(25.0f);
+				mouth.setStartAngle(0.0f);
+				mouth.setLength(180.0f);
+				break;
+			case "shock":
+				eye1.setRadiusY(15.0f);
+				eye2.setRadiusY(15.0f);
+				
+				mouth.setCenterX(200.0f);
+				mouth.setCenterY(135.0f);
+				mouth.setRadiusX(25.0f);
+				mouth.setRadiusY(25.0f);
+				mouth.setStartAngle(0.0f);
+				mouth.setLength(180.0f);
+				break;
+			case "wink":
+				eye1.setRadiusY(3.0f);
+							
+				mouth.setCenterX(180.0f);
+				mouth.setCenterY(110.0f);
+				mouth.setRadiusX(25.0f);
+				mouth.setRadiusY(25.0f);
+				mouth.setStartAngle(150.0f);
+				mouth.setLength(180.0f);
+				break;
+			default:
+				mouth.setCenterX(200.0f);
+				mouth.setCenterY(100.0f);
+				mouth.setRadiusX(25.0f);
+				mouth.setRadiusY(25.0f);
+				mouth.setStartAngle(180.0f);
+				mouth.setLength(180.0f);
+		}
 		
-		Arc mouth = new Arc();
-		mouth.setCenterX(200.0f);
-		mouth.setCenterY(100.0f);
-		mouth.setRadiusX(25.0f);
-		mouth.setRadiusY(25.0f);
-		mouth.setStartAngle(180.0f);
-		mouth.setLength(180.0f);
-		
+		setText(emote);
+				
 		getChildren().clear();
 		getChildren().add(body); 
 		getChildren().add(eye1); 
@@ -48,9 +105,74 @@ class MyMonster extends Pane {
 		getChildren().add(mouth); 
 	}
 	
-	public void setJoy(int j){
-		this.joy = j;
-		paint();
+	public String getEmote(){
+		return this.emote;
+	}
+		
+	public String getScore(){
+		this.setScore(this.score+1);
+		return String.valueOf(this.score);
+	}
+		
+	public String getQuestion(){
+		return this.question;
+	}
+	
+	public String getText(){
+		return this.text;
+	}
+		
+	public void setScore(int newScore){
+		this.score=newScore;
+	}
+	
+	public void newQuestion(){
+		if (questions.size()>0) {
+			int index = (int)System.currentTimeMillis() % this.questions.size();
+			
+			
+			String result = questions.get(index);
+			questions.remove(index);
+			
+			this.question=result;
+		}
+		else{
+			this.question="Please add some more questions.";	
+		}
+	}
+	
+	public void setNewQuestion(String newQuestion){
+		this.questions.add(newQuestion);
+	}
+	
+	public void setText(String emote){
+		int random = 0; 
+		
+		switch (emote){
+			case "sad":
+				random = (int)(Math.random() * 2);
+				if (random == 1){
+					this.text="W-what? Please take better care of yourself!";
+				}
+				else{
+					this.text="C-can you please take better care of yourself...?";
+				}
+				break;
+			case "shock":
+				random = (int)(Math.random() * 2);
+				if (random == 1){
+					this.text="What? Is there something you aren't telling me?";
+				}
+				else{
+					this.text="Huh? What do you mean by that?";
+				}
+				break;
+			case "wink":
+				this.text="Better get to it soon then!";
+				break;
+			default:
+				this.text="Yay! Nice job!";
+		}
 	}
 	
 	public void setEmote(String e){
